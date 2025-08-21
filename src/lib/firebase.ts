2781dda502f3a2c2ffe03ae1839e406c7d6a1b84
Anoop Patel <anooppbh8@gmail.com>
2025-08-21 15:01:12 +0000

    Error: [2025-08-21T14:46:03.262Z]  @firebase/database: "FIREBASE FATAL E

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 // Your web app's Firebase configuration
 // IMPORTANT: This is a public configuration and is safe to expose.
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyCunPeh6H5VjztyO0rFP_IVTWjJu1iasfo",
+  authDomain: "connect-9cfb7.firebaseapp.com",
+  databaseURL: "https://connect-9cfb7-default-rtdb.firebaseio.com/",
+  projectId: "connect-9cfb7",
+  storageBucket: "connect-9cfb7.appspot.com",
+  messagingSenderId: "431305405689",
+  appId: "1:431305405689:web:7145969ba83a7cdbed5e28"
 };
 
 // Initialize Firebase
