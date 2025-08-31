0c4809ad7fd77a4de0824554cc37c41eff7dbd98
Anoop Patel <anooppbh8@gmail.com>
2025-08-31 09:25:51 +0000

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -124,10 +124,10 @@ export default function RootLayout({
             type="application/ld+json"
             dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
-<<<<<<< HEAD
+
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2296905809539851"
-     crossorigin="anonymous"></script>
-=======
+     crossOrigin="anonymous"></script>
+
         {/* 
           To enable Google AdSense, you will need to place your ad code here.
           This typically involves adding a <script> tag provided by AdSense.
@@ -135,7 +135,7 @@ export default function RootLayout({
           <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-YOUR_CLIENT_ID"
                crossOrigin="anonymous" strategy="afterInteractive" />
         */}
->>>>>>> 6db16f5 (you add everythng for adssense policy)
+6db16f5 (you add everythng for adssense policy)
       </head>
       <body className={cn("font-body antialiased", inter.variable, spaceGrotesk.variable, homemadeApple.variable)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
